8fca48bb4ef4f497bd55741536ea5ec69cd42c14
juaneml <juanes.ml89@gmail.com>
2019-10-08 20:33:37 +0200

    modifico test

--- test.py
+++ test.py
@@ -10,16 +10,10 @@ import unittest
 
 
 class TestMethods(unittest.TestCase):
-
-    # with open('../json/datos_m.json','r') as noti:
-    #         lista_noticias = json.load(noti)
-
-    # pruebaNot = Newsgroups()
-    # pruebaNot.Crea_news(lista_noticias)
+    
     prueba = 2
     def prueba(self):
         self.assertEqual(self.prueba,2,"Resultado correcto")
         
-    
 if __name__ == '__main__':
     unittest.main()
