a318ee0e28a0d03b734a422fe39497c731ec8d51
juaneml <juanes.ml89@gmail.com>
2019-10-08 20:09:57 +0200

    añado fichero

---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import imp
+import pytest
+import unittest
+#sys.path.append('../src/')
+from principal import *
+
+
+class TestMethods(unittest.TestCase):
+
+    # with open('../json/datos_m.json','r') as noti:
+    #         lista_noticias = json.load(noti)
+
+    # pruebaNot = Newsgroups()
+    # pruebaNot.Crea_news(lista_noticias)
+    def prueba():
+        a = a +1
+    
+if __name__ == '__main__':
+    unittest.main()
