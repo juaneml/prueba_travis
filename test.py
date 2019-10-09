c0702224647ad1b4f108331e2033100d71f94e94
juaneml <juanes.ml89@gmail.com>
2019-10-09 16:32:46 +0200

    update test

--- test.py
+++ test.py
@@ -17,5 +17,31 @@ def inc(x):
 def test_answer():
     assert inc(3) == 4
         
+
+def conexion():
+    try:
+        connection = psycopg2.connect(user = "postgres",password = " ",host = "127.0.0.1",port = "5432",database = "travis_ci_test")
+
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print ( connection.get_dsn_parameters(),"\n")
+
+        # Print PostgreSQL version
+        #cursor.execute("select * from tblTest;")
+        record = cursor.fetchone()
+        #record = cursor.fetchall()
+        #pprint.pprint(record)
+        print("You are connected to - ", record,"\n")
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    return record
 if __name__ == '__main__':
     unittest.main()
