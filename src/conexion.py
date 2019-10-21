4e29b1f88c623209e0e5f4674d3f45aa3ff65ede
juaneml <juanes.ml89@gmail.com>
2019-10-22 01:04:16 +0200

    udpate travis-17

--- src/conexion.py
+++ src/conexion.py
@@ -5,7 +5,7 @@ import sys
 class Conexion:
 	def conexion(self):
 		try:
-            connection = psycopg2.connect(user = "postgres",password ="bar",host = "localhost",database="travis_ci_test")
+            connection = psycopg2.connect(user = "postgres",password ="bar",host = "localhost", database = "travis_ci_test")
 			#connection = psycopg2.connect(user = "postgres",password = "j9u4a1n",host = "127.0.0.1",database = "sistema")
             
 			cursor = connection.cursor()
